20835f40d2dd0817338f55a4c7817cab654790e5
shiina031 <ouka031@gmail.com>
2022-09-03 21:48:50 +0900

    gitアカウント連携確認

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { executeMergeSort } from './sort/mergeSort';
 import { executeQuickSort } from './sort/quicksort';
 
 executeQuickSort([7, 6, 10, 2, 5, 1, 8, 3, 9, 4], 10);
+executeMergeSort([7, 6, 10, 2, 5, 1, 8, 3, 9, 4], 10);
